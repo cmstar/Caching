678305e40d9d310e21bd86c7634d516676448b99
cmstar <cosmo_sei@hotmail.com>
2017-03-03 21:00:18 +0800

    ICacheIncreasable 和 ICacheFieldIncreasable 接口调整，不再支持浮点数增量处理了，浮点数有复杂的精度损失问题，容易导致各种意外情况。

--- cmstar.Caching/ICacheFieldIncreasable.cs
+++ cmstar.Caching/ICacheFieldIncreasable.cs
@@ -9,12 +9,13 @@ namespace cmstar.Caching
     {
         /// <summary>
         /// 当指定键的缓存存在时，将一个增量累加到具有指定键的缓存对象的的指定字段上。
+        /// 增量必须是整数，或能够转换为整数。
         /// </summary>
         /// <typeparam name="T">缓存对象的类型。</typeparam>
         /// <typeparam name="TField">字段的数据类型。</typeparam>
         /// <param name="key">缓存的键。</param>
         /// <param name="field">字段名称。</param>
-        /// <param name="increment">增量。使用负数来做减法。</param>
+        /// <param name="increment">增量，必须是整数，或能够转换为整数。使用负数来做减法。</param>
         /// <returns>
         /// 若对已存在的缓存对象的对应字段进行了设值，返回增加后的字段值；
         /// 否则返回<typeparamref name="TField"/>的默认值。
@@ -24,12 +25,13 @@ namespace cmstar.Caching
         /// <summary>
         /// 将一个增量累加到具有指定键的缓存对象的的指定字段上。
         /// 若对象或字段不存在，尝试创建对象或字段。
+        /// 增量必须是整数，或能够转换为整数。
         /// </summary>
         /// <typeparam name="T">缓存对象的类型。</typeparam>
         /// <typeparam name="TField">字段的数据类型。</typeparam>
         /// <param name="key">缓存的键。</param>
         /// <param name="field">字段名称。</param>
-        /// <param name="increment">增量。使用负数来做减法。</param>
+        /// <param name="increment">增量，必须是整数，或能够转换为整数。使用负数来做减法。</param>
         /// <param name="expiration">
         /// 指定被创建的缓存对象的过期时间。使用<see cref="TimeSpan.Zero"/>表示缓存不会过期。
         /// </param>
--- cmstar.Caching/ICacheIncreasable.cs
+++ cmstar.Caching/ICacheIncreasable.cs
@@ -9,20 +9,22 @@ namespace cmstar.Caching
     {
         /// <summary>
         /// 当指定键的缓存存在时，将一个增量累加到该缓存的值上。
+        /// 增量必须是整数，或能够转换为整数。
         /// </summary>
         /// <typeparam name="T">缓存对象的类型。</typeparam>
         /// <param name="key">缓存的键。</param>
-        /// <param name="increment">增量。使用负数来做减法。</param>
+        /// <param name="increment">增量，必须是整数，或能够转换为整数。使用负数来做减法。</param>
         /// <returns>指定键的缓存存在时，返回增加后的值；否则返回<typeparamref name="T"/>的默认值。</returns>
         T Increase<T>(string key, T increment);
 
         /// <summary>
         /// 当指定键的缓存存在时，将一个增量累加到该缓存的值上。
-        /// 若缓存不存在，则初始化其值为增量值。
+        /// 若缓存不存在，则初始化其值为增量值，并具有给定的过期时间。
+        /// 增量必须是整数，或能够转换为整数。
         /// </summary>
         /// <typeparam name="T">缓存对象的类型。</typeparam>
         /// <param name="key">缓存的键。</param>
-        /// <param name="increment">增量。使用负数来做减法。</param>
+        /// <param name="increment">增量，必须是整数，或能够转换为整数。使用负数来做减法。</param>
         /// <param name="expiration">
         /// 指定被创建的缓存对象的过期时间。使用<see cref="TimeSpan.Zero"/>表示缓存不会过期。
         /// </param>
