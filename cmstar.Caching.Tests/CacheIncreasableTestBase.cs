e4656707dd1e8e656323fb88344d75697ec62023
cmstar <cosmo_sei@hotmail.com>
2017-03-06 20:04:42 +0800

    完善 ICacheIncreasable 的单元测试，增加了每此调用方法后的返回值断言，同时简化了代码。

--- cmstar.Caching.Tests/CacheIncreasableTestBase.cs
+++ cmstar.Caching.Tests/CacheIncreasableTestBase.cs
@@ -15,205 +15,152 @@ namespace cmstar.Caching
         protected abstract ICacheIncreasable CacheProvider { get; }
 
         [SetUp]
-        public void SetUp()
-        {
-            Console.WriteLine("Remove the old key...");
-            CacheProvider.Remove(Key);
-        }
-
         [TearDown]
-        public void TearDown()
+        public void SetUp()
         {
             Console.WriteLine("Cleanup...");
             CacheProvider.Remove(Key);
-            NotExist(Key);
+            AssertCacheNotExist();
         }
 
         [Test]
         public void TestOnInt64()
         {
-            Console.WriteLine("Test INCREASE on not-existing key...");
-            CacheProvider.Increase<long>(Key, 1);
-            CacheProvider.Increase<long>(Key, 100);
-            NotExist(Key);
+            Console.WriteLine("Test INCREASE on non-existing key...");
+            Assert.AreEqual(0, CacheProvider.Increase<long>(Key, 1));
+            Assert.AreEqual(0, CacheProvider.Increase<long>(Key, 100));
+            AssertCacheNotExist();
 
             Console.WriteLine("Test expiration control for INCREASEORCREATE...");
-            CacheProvider.IncreaseOrCreate<long>(Key, 1, ExpiryShort);
-            EnsureCacheValue(Key, 1);
+            Assert.AreEqual(1, CacheProvider.IncreaseOrCreate<long>(Key, 1, ExpiryShort));
+            AssertCacheValue(1);
             Thread.Sleep(ExpiryShort.Add(TimeSpan.FromMilliseconds(50)));
-            NotExist(Key);
+            AssertCacheNotExist();
 
             Console.WriteLine("Test INCREASEORCREATE...");
-            CacheProvider.IncreaseOrCreate<long>(Key, 1, ExpiryLong);
-            EnsureCacheValue(Key, 1);
-
-            CacheProvider.IncreaseOrCreate<long>(Key, -5, ExpiryLong);
-            EnsureCacheValue(Key, -4);
-
-            CacheProvider.IncreaseOrCreate<long>(Key, 10004, ExpiryLong);
-            EnsureCacheValue(Key, 10000);
-
-            CacheProvider.IncreaseOrCreate<long>(Key, -10000, ExpiryLong);
-            EnsureCacheValue(Key, 0);
+            Func<long, long> increaseOrCreate = x => CacheProvider.IncreaseOrCreate(Key, x, ExpiryLong);
+            ActionAndAssert<long>(increaseOrCreate, 1, 1);
+            ActionAndAssert<long>(increaseOrCreate, -5, -4);
+            ActionAndAssert<long>(increaseOrCreate, 10004, 10000);
+            ActionAndAssert<long>(increaseOrCreate, -10000, 0);
 
             Console.WriteLine("Test INCREASE...");
-            CacheProvider.Increase<long>(Key, 1);
-            EnsureCacheValue(Key, 1);
-
-            CacheProvider.Increase<long>(Key, 1);
-            EnsureCacheValue(Key, 2);
-
-            CacheProvider.Increase<long>(Key, 12356);
-            EnsureCacheValue(Key, 12358);
-
-            CacheProvider.Increase<long>(Key, -5);
-            EnsureCacheValue(Key, 12353);
-
-            CacheProvider.Increase<long>(Key, -33333);
-            EnsureCacheValue(Key, -20980);
-
-            CacheProvider.Increase<long>(Key, 0);
-            EnsureCacheValue(Key, -20980);
+            Func<long, long> increase = x => CacheProvider.Increase(Key, x);
+            ActionAndAssert<long>(increase, 1, 1);
+            ActionAndAssert<long>(increase, 1, 2);
+            ActionAndAssert<long>(increase, 12356, 12358);
+            ActionAndAssert<long>(increase, -5, 12353);
+            ActionAndAssert<long>(increase, -33333, -20980);
+            ActionAndAssert<long>(increase, 0, -20980);
         }
 
         [Test]
         public void TestOnInt32()
         {
-            Console.WriteLine("Test INCREASE on not-existing key...");
-            CacheProvider.Increase<int>(Key, 1);
-            CacheProvider.Increase<int>(Key, 100);
-            NotExist(Key);
+            Console.WriteLine("Test INCREASE on non-existing key...");
+            Assert.AreEqual(0, CacheProvider.Increase<int>(Key, 1));
+            Assert.AreEqual(0, CacheProvider.Increase<int>(Key, 100));
+            AssertCacheNotExist();
 
             Console.WriteLine("Test expiration control for INCREASEORCREATE...");
-            CacheProvider.IncreaseOrCreate<int>(Key, 1, ExpiryShort);
-            EnsureCacheValue(Key, 1);
+            Assert.AreEqual(1, CacheProvider.IncreaseOrCreate<int>(Key, 1, ExpiryShort));
+            AssertCacheValue(1);
             Thread.Sleep(ExpiryShort.Add(TimeSpan.FromMilliseconds(50)));
-            NotExist(Key);
+            AssertCacheNotExist();
 
             Console.WriteLine("Test INCREASEORCREATE...");
-            CacheProvider.IncreaseOrCreate<int>(Key, 1, ExpiryLong);
-            EnsureCacheValue(Key, 1);
-
-            CacheProvider.IncreaseOrCreate<int>(Key, -5, ExpiryLong);
-            EnsureCacheValue(Key, -4);
-
-            CacheProvider.IncreaseOrCreate<int>(Key, 10004, ExpiryLong);
-            EnsureCacheValue(Key, 10000);
-
-            CacheProvider.IncreaseOrCreate<int>(Key, -10000, ExpiryLong);
-            EnsureCacheValue(Key, 0);
+            Func<int, int> increaseOrCreate = x => CacheProvider.IncreaseOrCreate(Key, x, ExpiryLong);
+            ActionAndAssert<int>(increaseOrCreate, 1, 1);
+            ActionAndAssert<int>(increaseOrCreate, -5, -4);
+            ActionAndAssert<int>(increaseOrCreate, 10004, 10000);
+            ActionAndAssert<int>(increaseOrCreate, -10000, 0);
 
             Console.WriteLine("Test INCREASE...");
-            CacheProvider.Increase<int>(Key, 1);
-            EnsureCacheValue(Key, 1);
-
-            CacheProvider.Increase<int>(Key, 1);
-            EnsureCacheValue(Key, 2);
-
-            CacheProvider.Increase<int>(Key, 12356);
-            EnsureCacheValue(Key, 12358);
-
-            CacheProvider.Increase<int>(Key, -5);
-            EnsureCacheValue(Key, 12353);
-
-            CacheProvider.Increase<int>(Key, -33333);
-            EnsureCacheValue(Key, -20980);
-
-            CacheProvider.Increase<int>(Key, 0);
-            EnsureCacheValue(Key, -20980);
+            Func<int, int> increase = x => CacheProvider.Increase(Key, x);
+            ActionAndAssert<int>(increase, 1, 1);
+            ActionAndAssert<int>(increase, 1, 2);
+            ActionAndAssert<int>(increase, 12356, 12358);
+            ActionAndAssert<int>(increase, -5, 12353);
+            ActionAndAssert<int>(increase, -33333, -20980);
+            ActionAndAssert<int>(increase, 0, -20980);
         }
 
         [Test]
         public void TestOnInt16()
         {
-            Console.WriteLine("Test INCREASE on not-existing key...");
-            CacheProvider.Increase<short>(Key, 1);
-            CacheProvider.Increase<short>(Key, 100);
-            NotExist(Key);
+            Console.WriteLine("Test INCREASE on non-existing key...");
+            Assert.AreEqual(0, CacheProvider.Increase<short>(Key, 1));
+            Assert.AreEqual(0, CacheProvider.Increase<short>(Key, 100));
+            AssertCacheNotExist();
 
             Console.WriteLine("Test expiration control for INCREASEORCREATE...");
-            CacheProvider.IncreaseOrCreate<short>(Key, 1, ExpiryShort);
-            EnsureCacheValue(Key, 1);
+            Assert.AreEqual(1, CacheProvider.IncreaseOrCreate<short>(Key, 1, ExpiryShort));
+            AssertCacheValue(1);
             Thread.Sleep(ExpiryShort.Add(TimeSpan.FromMilliseconds(50)));
-            NotExist(Key);
+            AssertCacheNotExist();
 
             Console.WriteLine("Test INCREASEORCREATE...");
-            CacheProvider.IncreaseOrCreate<short>(Key, 1, ExpiryLong);
-            EnsureCacheValue(Key, 1);
-
-            CacheProvider.IncreaseOrCreate<short>(Key, -5, ExpiryLong);
-            EnsureCacheValue(Key, -4);
-
-            CacheProvider.IncreaseOrCreate<short>(Key, 10004, ExpiryLong);
-            EnsureCacheValue(Key, 10000);
-
-            CacheProvider.IncreaseOrCreate<short>(Key, -10000, ExpiryLong);
-            EnsureCacheValue(Key, 0);
+            Func<short, short> increaseOrCreate = x => CacheProvider.IncreaseOrCreate(Key, x, ExpiryLong);
+            ActionAndAssert<short>(increaseOrCreate, 1, 1);
+            ActionAndAssert<short>(increaseOrCreate, -5, -4);
+            ActionAndAssert<short>(increaseOrCreate, 10004, 10000);
+            ActionAndAssert<short>(increaseOrCreate, -10000, 0);
 
             Console.WriteLine("Test INCREASE...");
-            CacheProvider.Increase<short>(Key, 1);
-            EnsureCacheValue(Key, 1);
-
-            CacheProvider.Increase<short>(Key, 1);
-            EnsureCacheValue(Key, 2);
-
-            CacheProvider.Increase<short>(Key, 12356);
-            EnsureCacheValue(Key, 12358);
-
-            CacheProvider.Increase<short>(Key, -5);
-            EnsureCacheValue(Key, 12353);
-
-            CacheProvider.Increase<short>(Key, -12353);
-            EnsureCacheValue(Key, 0);
-
-            CacheProvider.Increase<short>(Key, 0);
-            EnsureCacheValue(Key, 0);
+            Func<short, short> increase = x => CacheProvider.Increase(Key, x);
+            ActionAndAssert<short>(increase, 1, 1);
+            ActionAndAssert<short>(increase, 1, 2);
+            ActionAndAssert<short>(increase, 12356, 12358);
+            ActionAndAssert<short>(increase, -5, 12353);
+            ActionAndAssert<short>(increase, -12353, 0);
+            ActionAndAssert<short>(increase, 0, 0);
         }
 
         [Test]
         public void TestOnByte()
         {
-            Console.WriteLine("Test INCREASE on not-existing key...");
-            CacheProvider.Increase<byte>(Key, 1);
-            CacheProvider.Increase<byte>(Key, 100);
-            NotExist(Key);
+            Console.WriteLine("Test INCREASE on non-existing key...");
+            Assert.AreEqual(0, CacheProvider.Increase<byte>(Key, 1));
+            Assert.AreEqual(0, CacheProvider.Increase<byte>(Key, 100));
+            AssertCacheNotExist();
 
             Console.WriteLine("Test expiration control for INCREASEORCREATE...");
             CacheProvider.IncreaseOrCreate<byte>(Key, 1, ExpiryShort);
-            EnsureCacheValue(Key, 1);
+            AssertCacheValue(1);
             Thread.Sleep(ExpiryShort.Add(TimeSpan.FromMilliseconds(50)));
-            NotExist(Key);
+            AssertCacheNotExist();
 
             Console.WriteLine("Test INCREASEORCREATE...");
-            CacheProvider.IncreaseOrCreate<byte>(Key, 1, ExpiryLong);
-            EnsureCacheValue(Key, 1);
-
-            CacheProvider.IncreaseOrCreate<byte>(Key, 3, ExpiryLong);
-            EnsureCacheValue(Key, 4);
+            Func<byte, byte> increaseOrCreate = x => CacheProvider.IncreaseOrCreate(Key, x, ExpiryLong);
+            ActionAndAssert<byte>(increaseOrCreate, 1, 1);
+            ActionAndAssert<byte>(increaseOrCreate, 3, 4);
 
             Console.WriteLine("Test INCREASE...");
-            CacheProvider.Increase<byte>(Key, 1);
-            EnsureCacheValue(Key, 5);
-
-            CacheProvider.Increase<byte>(Key, 1);
-            EnsureCacheValue(Key, 6);
-
-            CacheProvider.Increase<byte>(Key, 33);
-            EnsureCacheValue(Key, 39);
-
-            CacheProvider.Increase<byte>(Key, 0);
-            EnsureCacheValue(Key, 39);
+            Func<byte, byte> increase = x => CacheProvider.Increase(Key, x);
+            ActionAndAssert<byte>(increase, 1, 5);
+            ActionAndAssert<byte>(increase, 1, 6);
+            ActionAndAssert<byte>(increase, 33, 39);
+            ActionAndAssert<byte>(increase, 0, 39);
         }
 
-        private void NotExist(string key)
+        private void AssertCacheNotExist()
         {
-            Assert.IsFalse(CacheProvider.Remove(key));
+            Assert.IsFalse(CacheProvider.Remove(Key));
         }
 
-        private void EnsureCacheValue(string key, long expected)
+        private void AssertCacheValue(long expected)
         {
-            var cachedValue = CacheProvider.Get<long>(key);
+            var cachedValue = CacheProvider.Get<long>(Key);
             Assert.AreEqual(expected, cachedValue);
+        }
+
+        private void ActionAndAssert<T>(Func<T, T> funcIncr, T increment, T expected)
+        {
+            var res = funcIncr(increment);
+            Assert.AreEqual(expected, res);
+
+            res = CacheProvider.Get<T>(Key);
+            Assert.AreEqual(expected, res);
         }
     }
 }
