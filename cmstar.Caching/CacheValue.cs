6f25a522b4c41d136109c9aab81040166d7f14a8
cmstar <cosmo_sei@hotmail.com>
2020-10-10 14:00:17 +0800

    添加一段注释。

--- cmstar.Caching/CacheValue.cs
+++ cmstar.Caching/CacheValue.cs
@@ -3,6 +3,9 @@
     /// <summary>
     /// 存储缓存的值。
     /// </summary>
+    /// <remarks>
+    /// <see cref="Value"/>可能被直接修改，当前类型必须是 class ，不能是 struct 。
+    /// </remarks>
     internal class CacheValue
     {
         public CacheValue(object value)
