bdd29871d8e6d04b5a32f575e5a860633580c47f
cmstar <cosmo_sei@hotmail.com>
2022-05-11 23:53:00 +0800

    更新 NuGet 元数据，增加打包脚本。

---
+++ pack.bat
@@ -0,0 +1,7 @@
+SET cur_dir=%~dp0
+SET output=%cur_dir%.build
+
+SET build_cmd=dotnet pack -c Release --output %output%
+%build_cmd% %cur_dir%cmstar.Caching
+%build_cmd% %cur_dir%cmstar.Caching.Redis
+
